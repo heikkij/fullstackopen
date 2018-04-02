e3ed4e7c81f68836244e39e3239cd7e6fe638886
Heikki J <heikkij@rocketmail.com>
2018-04-02 11:21:34 +0300

    osa1 - komponentti

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => {
+    const now = new Date()
+    const a = 10
+    const b = 20
+    return (
+      <div>
+        <p>Hello world, it is {now.toString()}</p>
+        <p>{a} plus {b} is {a + b}</p>
+      </div>
+    )
+  }
+
+ReactDOM.render(<App />, document.getElementById('root'))
