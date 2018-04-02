adf775e0d69633bb844419f9377ea6c2e3cc3ba2
Heikki J <heikkij@rocketmail.com>
2018-04-02 11:57:12 +0300

    tehtävät 1.1 ja 1.2

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,23 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-  const Hello = (props) => {
+const Otsikko = (props) => {
     return (
-      <div>
-        <p>Hello {props.name}, you are {props.age} years old</p>
-      </div>
+        <div>
+            <h1>{props.kurssi}</h1>
+        </div>
     )
-  }
-  
-  const App = () => {
-    const nimi = 'Pekka'
-    const ika = 10
+}
+
+const Osa = (props) => {
     return (
-      <div>
-        <h1>Greetings</h1>
-        <Hello name="Arto" age={26 + 10} />
-        <Hello name={nimi} age={ika} />
-      </div>
+        <div>
+            <p>{props.osa} {props.tehtavia}</p>
+        </div>
     )
-  }
-  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+const Sisalto = (props) => {
+    return (
+        <div>
+            <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
+            <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
+            <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
+        </div>
+    )
+}
+
+const Yhteensa = (props) => {
+    return (
+        <div>
+            <p>yhteensä {props.tehtavia} tehtävää</p>
+        </div>
+    )
+
+}
+
+const App = () => {
+  const kurssi = 'Half Stack -sovelluskehitys'
+  const osa1 = 'Reactin perusteet'
+  const tehtavia1 = 10
+  const osa2 = 'Tiedonvälitys propseilla'
+  const tehtavia2 = 7
+  const osa3 = 'Komponenttien tila'
+  const tehtavia3 = 14
+
+  return (
+    <div>
+      <Otsikko kurssi={kurssi} />
+      <Sisalto osa1={osa1} tehtavia1={tehtavia1} osa2={osa2} tehtavia2={tehtavia2} osa3={osa3} tehtavia3={tehtavia3} />
+      <Yhteensa tehtavia={tehtavia1 + tehtavia2 + tehtavia3} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
